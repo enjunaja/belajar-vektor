4a02ba79b5c51cc0cf6076d7abbf1c398c3c2b42
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-10-31 06:42:13 +0700

    Update menu_kalkulator.py

--- menu_kalkulator.py
+++ menu_kalkulator.py
@@ -4,28 +4,27 @@ import numpy as np
 def kalkulator_menu():
   st.header("Kalkulator Perkalian Vektor")
   
-  def main():
-    st.title('Kalkulator Perkalian Vektor')
+    def hitung():
+   
+      # Input vektor pertama
+      st.header('Vektor Pertama')
+      vector1 = st.text_input('Masukkan vektor pertama (pisahkan angka dengan spasi):')
 
-    # Input vektor pertama
-    st.header('Vektor Pertama')
-    vector1 = st.text_input('Masukkan vektor pertama (pisahkan angka dengan spasi):')
+      # Input vektor kedua
+      st.header('Vektor Kedua')
+      vector2 = st.text_input('Masukkan vektor kedua (pisahkan angka dengan spasi):')
 
-    # Input vektor kedua
-    st.header('Vektor Kedua')
-    vector2 = st.text_input('Masukkan vektor kedua (pisahkan angka dengan spasi):')
+      # Konversi input ke dalam bentuk list
+      vector1 = [float(x) for x in vector1.split()]
+      vector2 = [float(x) for x in vector2.split()]
 
-    # Konversi input ke dalam bentuk list
-    vector1 = [float(x) for x in vector1.split()]
-    vector2 = [float(x) for x in vector2.split()]
+      # Hitung perkalian vektor
+      result = np.dot(vector1, vector2)
 
-    # Hitung perkalian vektor
-    result = np.dot(vector1, vector2)
+      # Tampilkan hasil
+      st.header('Hasil Perkalian Vektor')
+      st.write(f'Vektor Pertama: {vector1}')
+      st.write(f'Vektor Kedua: {vector2}')
+      st.write(f'Hasil Perkalian: {result}')
 
-    # Tampilkan hasil
-    st.header('Hasil Perkalian Vektor')
-    st.write(f'Vektor Pertama: {vector1}')
-    st.write(f'Vektor Kedua: {vector2}')
-    st.write(f'Hasil Perkalian: {result}')
-
-  main()
+   hitung()

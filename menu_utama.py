40c924977c9056ac6157189401aff3348536f552
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-10-30 20:05:11 +0700

    Create menu_utama.py

---
+++ menu_utama.py
@@ -0,0 +1,25 @@
+# main_app.py
+
+import streamlit as st
+from menu_beranda import beranda_menu
+from menu_tentang import tentang_menu
+from menu_kontak import kontak_menu
+
+# Judul aplikasi
+st.title("Belajar Ruang Vektor")
+
+# Pilihan menu dalam sidebar
+menu = st.sidebar.selectbox("Menu", ["Beranda", "Tentang", "Kalkulator", "Kontak"])
+
+# Tampilkan konten sesuai dengan pilihan menu
+if menu == "Beranda":
+    beranda_menu()
+
+elif menu == "Tentang":
+    tentang_menu()
+
+elif menu == "Kalkulator":
+    kalkulator_menu()
+
+elif menu == "Kontak":
+    kontak_menu()

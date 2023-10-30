a8ea394b62e56a69d6df080ca6a370e113096df1
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-10-30 19:34:16 +0700

    Update SelamatDatang.py

--- SelamatDatang.py
+++ SelamatDatang.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import numpy as np
 
+def main():
+    st.title('Kalkulator Perkalian Vektor')
+
+    # Input vektor pertama
+    st.header('Vektor Pertama')
+    vector1 = st.text_input('Masukkan vektor pertama (pisahkan angka dengan spasi):')
+
+    # Input vektor kedua
+    st.header('Vektor Kedua')
+    vector2 = st.text_input('Masukkan vektor kedua (pisahkan angka dengan spasi):')
+
+    # Konversi input ke dalam bentuk list
+    vector1 = [float(x) for x in vector1.split()]
+    vector2 = [float(x) for x in vector2.split()]
+
+    # Hitung perkalian vektor
+    result = np.dot(vector1, vector2)
+
+    # Tampilkan hasil
+    st.header('Hasil Perkalian Vektor')
+    st.write(f'Vektor Pertama: {vector1}')
+    st.write(f'Vektor Kedua: {vector2}')
+    st.write(f'Hasil Perkalian: {result}')
+
+if __name__ == '__main__':
+    main()

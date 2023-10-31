cd075413af2e8918092cc260666fff22622a927f
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-10-31 07:27:48 +0700

    Update menu_tentang.py

--- menu_tentang.py
+++ menu_tentang.py
@@ -3,9 +3,11 @@
 import streamlit as st
 
 def tentang_menu():
-    st.header("Tentang Kami")
+    st.subheader("Tentang Kami")
+    st.write(" ")
     st.write("Kami adalah tim pengampu Mata Kuliah Aljabar Linier dan Matriks!")
     st.write("Konten dan Programmer 👩‍🏫: Enjun Junaeti")
     st.write("Supervisor 👨‍⚖️: Prof. H. Yaya Sukjaya Kusumah, M.Sc., Ph. D.")
+    st.write(" ")
     st.write("Program Studi Pendidikan Ilmu Komputer")
     st.write("FPMIPA UPI")

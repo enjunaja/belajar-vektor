2403fb700498737e43d0d2501f4676ca4942d41f
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 17:03:14 +0700

    Update menu_beranda.py

--- menu_beranda.py
+++ menu_beranda.py
@@ -6,4 +6,4 @@ def beranda_menu():
     st.write("")
     st.markdown("<h4 style='text-align: left;'>Selamat datang di Beranda 👋</h4>", unsafe_allow_html=True)
     st.write("Ini adalah halaman beranda aplikasi kami.
-    👈 Pilih Menu dari Sidebar untuk memilih kegiatan yang akan dilakukan.")
+     👈 Pilih Menu dari Sidebar untuk memilih kegiatan yang akan dilakukan.")

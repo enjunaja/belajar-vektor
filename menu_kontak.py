188df46ca92e4eeb11bbc0b30575720299eaf7e4
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 18:00:05 +0700

    Update menu_kontak.py

--- menu_kontak.py
+++ menu_kontak.py
@@ -11,5 +11,5 @@ def kontak_menu():
     st.write("2. Tuliskan hasil diskusi pada tempat yang telah disediakan.")
     st.write(" ")
     st.markdown("<h6 style='text-align: center;'>========================================================================</h4>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center;'>Merepresentasikan posisi dan perpindahan sebagai vektor dalam bentuk geometri bidang dan ruang</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center;'>Merepresentasikan Posisi dan Perpindahan sebagai Vektor</h4>", unsafe_allow_html=True)
     st.markdown("<h6 style='text-align: center;'>========================================================================</h4>", unsafe_allow_html=True)

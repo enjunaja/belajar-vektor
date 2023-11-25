026bb55747604ca3506c1ca271df57c539dcd1db
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 17:04:37 +0700

    Update menu_beranda.py

--- menu_beranda.py
+++ menu_beranda.py
@@ -5,5 +5,11 @@ import streamlit as st
 def beranda_menu():
     st.write("")
     st.markdown("<h4 style='text-align: left;'>Selamat datang di Beranda 👋</h4>", unsafe_allow_html=True)
-    st.write("Ini adalah halaman beranda aplikasi kami.
-     👈 Pilih Menu dari Sidebar untuk memilih kegiatan yang akan dilakukan.")
+
+    st.markdown(
+        """
+        Ini adalah halaman beranda aplikasi kami.
+        👈 Pilih Menu dari Sidebar untuk memilih kegiatan yang akan dilakukan.
+
+        """
+    )

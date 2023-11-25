b57159b56a72d17828543494ca3d9c35ce540503
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 18:49:36 +0700

    Update menu_kontak.py

--- menu_kontak.py
+++ menu_kontak.py
@@ -19,10 +19,10 @@ def kontak_menu():
     st.markdown("<h6 style='text-align: center;'>==================================================================================</h4>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center;'>Merepresentasikan Posisi dan Perpindahan sebagai Vektor</h4>", unsafe_allow_html=True)
     st.markdown("<h6 style='text-align: center;'>==================================================================================</h4>", unsafe_allow_html=True)
-
+    
     st.markdown("<h5 style='text-align: left;'>Tahapan Pembelajaran</h4>", unsafe_allow_html=True)
     st.write("Perhatikan gambar berikut!")
     url_github = "https://github.com/enjunaja/belajar-vektor/blob/main/gambar1.png?raw=true"
     st.image(url_github, caption='Gambar dari Buku', use_column_width=True)
-
+    
    st.write("Seorang atlet golf akan memukul bola yang berada pada posisi (0,0) seperti gambar diatas dengan tujuan adalah posisi lubang bola golf yang ditandai bendera. Bagaimana kita dapat menyatakan posisi lubang bola golf terhadap titik awal bola?")

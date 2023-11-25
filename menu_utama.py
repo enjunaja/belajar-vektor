07bce6757835dc274f5bb4881b26d0326def2771
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-26 05:39:52 +0700

    Update menu_utama.py

--- menu_utama.py
+++ menu_utama.py
@@ -3,11 +3,11 @@
 import streamlit as st
 from menu_beranda import beranda_menu
 from menu_tentang import tentang_menu
-from menu_kalkulator import kalkulator_menu
-from menu_kontak import kontak_menu
+from menu_konsep import konsep_menu
+from menu_lkm import lkm_menu
 
 # Pilihan menu dalam sidebar
-menu = st.sidebar.selectbox("Menu", ["Beranda", "Tentang", "Belajar", "Lembar Kerja"])
+menu = st.sidebar.selectbox("Menu", ["Beranda", "Tentang", "Konsep Materi", "Lembar Kerja"])
 
 # Tampilkan konten sesuai dengan pilihan menu
 if menu == "Beranda":
@@ -16,8 +16,8 @@ if menu == "Beranda":
 elif menu == "Tentang":
     tentang_menu()
 
-elif menu == "Belajar":
-    kalkulator_menu()
+elif menu == "Konsep Materi":
+    konsep_menu()
 
 elif menu == "Lembar Kerja":
-    kontak_menu()
+    lkm_menu()

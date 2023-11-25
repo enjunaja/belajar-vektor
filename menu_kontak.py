628bc3eb914c20c41af62df636f67471e50ecaa1
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 17:57:06 +0700

    Update menu_kontak.py

--- menu_kontak.py
+++ menu_kontak.py
@@ -3,8 +3,13 @@
 import streamlit as st
 
 def kontak_menu():
-    st.markdown("<h3 style='text-align: center;'> Ruang Vektor </h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'> Lembar Kerja Mahasiswa </h3>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'> 💻 Vektor sebagai Representasi Data</h3>", unsafe_allow_html=True)
     st.write("")
-    st.markdown("<h4 style='text-align: left;'>Kontak Kami</h4>", unsafe_allow_html=True)
-    st.write("📧 Email: enjun@upi.edu")
+    st.markdown("<h5 style='text-align: left;'>Petunjuk Pembelajaran</h4>", unsafe_allow_html=True)
+    st.write("1. Lakukan diskusi bersama dengan teman sekelompok untuk menjawab pertanyaan-pertanyaan berikut ini.")
+    st.write("2. Tuliskan hasil diskusi pada tempat yang telah disediakan.")
+    st.write(" ")
+    st.markdown("<h6 style='text-align: center;'>========================================================================</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center;'>Merepresentasikan posisi dan perpindahan sebagai vektor dalam bentuk geometri bidang dan ruang</h4>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'>========================================================================</h4>", unsafe_allow_html=True)

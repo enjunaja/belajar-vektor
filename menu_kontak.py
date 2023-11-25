1da4a1d52f4b096b4cb9c6eee70bc1d0b140fd72
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 19:07:41 +0700

    Update menu_kontak.py

--- menu_kontak.py
+++ menu_kontak.py
@@ -1,6 +1,7 @@
 # menu_kontak.py
 
 import streamlit as st
+from PIL import Image
 
 def kontak_menu():
     st.markdown("<h3 style='text-align: center;'> Lembar Kerja Mahasiswa </h3>", unsafe_allow_html=True)
@@ -23,6 +24,6 @@ def kontak_menu():
     st.markdown("<h5 style='text-align: left;'>Tahapan Pembelajaran</h4>", unsafe_allow_html=True)
     st.write("Perhatikan gambar berikut!")
     url_github = "https://github.com/enjunaja/belajar-vektor/blob/main/gambar1.png?raw=true"
-    st.image(url_github, caption='Gambar dari Buku', use_column_width=True)
+    st.image(url_github, caption='Gambar dari Buku')
     st.write("Seorang atlet golf akan memukul bola yang berada pada posisi (0,0) seperti gambar diatas dengan tujuan adalah posisi lubang bola golf yang ditandai bendera. Bagaimana kita dapat menyatakan posisi lubang bola golf terhadap titik awal bola?")
     jawab1 = st.text_input('Jawab:')

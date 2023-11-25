2582648f18c9745eb724280c8200e7ba39caa35b
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 17:25:17 +0700

    Update menu_beranda.py

--- menu_beranda.py
+++ menu_beranda.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 def beranda_menu():
     # Judul aplikasi
-    st.markdown("<h3 style='text-align: center;'> 💻 Ruang Vektor </h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'> Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'> Ruang Vektor </h3>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
     st.write("")
     st.markdown("<h4 style='text-align: left;'>Selamat datang di Beranda 👋</h4>", unsafe_allow_html=True)
 

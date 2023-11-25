6b382ab1cd504ece5d498a56180f7809cdca1101
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-25 17:25:46 +0700

    Update menu_kontak.py

--- menu_kontak.py
+++ menu_kontak.py
@@ -3,8 +3,8 @@
 import streamlit as st
 
 def kontak_menu():
-    st.markdown("<h3 style='text-align: center;'> 💻 Ruang Vektor </h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'> Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'> Ruang Vektor </h3>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
     st.write("")
     st.markdown("<h4 style='text-align: left;'>Kontak Kami</h4>", unsafe_allow_html=True)
     st.write("📧 Email: enjun@upi.edu")

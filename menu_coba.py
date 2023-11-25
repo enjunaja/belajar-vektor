16dfe05e2c22908c2a8ce247eab7871dbf3def9e
enjunaja <95700781+enjunaja@users.noreply.github.com>
2023-11-26 06:07:42 +0700

    Create menu_coba.py

---
+++ menu_coba.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def coba_menu():
+    # Judul aplikasi
+    st.markdown("<h3 style='text-align: center;'> Ini menu untuk coba program </h3>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
+    st.write("")
+    st.markdown("<h4 style='text-align: left;'>Silahkan tulis kode programnya</h4>", unsafe_allow_html=True)
+
+    

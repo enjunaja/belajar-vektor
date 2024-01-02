5d76f73265fe65f68ae3de206f1a1a642b2c9a3d
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 10:50:42 +0700

    Update menu_beranda.py

--- menu/menu_beranda.py
+++ menu/menu_beranda.py
@@ -6,7 +6,7 @@ def beranda_menu():
     # Judul aplikasi
     st.markdown("<h3 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor</h3>", unsafe_allow_html=True)
     st.write("")
-    st.markdown("<h4 style='text-align: left;'>Selamat datang di Beranda 👋</h4>", unsafe_allow_html=True)
+    st.sidebar.image("https://https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/Webinar-pana.png",width=2000)
 
     st.markdown(
         """

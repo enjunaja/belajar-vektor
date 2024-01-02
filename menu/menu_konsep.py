e5ede294209ecfceab1290a2b7bf24e4e1c1025e
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 12:05:44 +0700

    Update menu_konsep.py

--- menu/menu_konsep.py
+++ menu/menu_konsep.py
@@ -2,7 +2,18 @@ def konsep_menu():
   import streamlit as st
   import numpy as np
 
+  # Mendefinisikan CSS untuk menyesuaikan ukuran teks di tab
+  custom_tab_style = (
+    "display: flex; justify-content: center; align-items: center; "
+    "font-size: 24px;"  # Sesuaikan ukuran teks sesuai kebutuhan Anda
+  )
 
+  # Menampilkan tab dengan gaya khusus
+  st.markdown(
+    f'<div style="{custom_tab_style}">My Tab</div>',
+    unsafe_allow_html=True
+  )
+  
   st.markdown("<h2 style='text-align: center;'> 💻 Vektor sebagai Representasi Data</h2>", unsafe_allow_html=True)
   st.write("")
 

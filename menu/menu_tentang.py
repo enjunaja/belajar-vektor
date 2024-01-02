1101e425b69c3804e8603fb0df44b7269a0d0411
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 11:56:58 +0700

    Update menu_tentang.py

--- menu/menu_tentang.py
+++ menu/menu_tentang.py
@@ -3,14 +3,13 @@
 import streamlit as st
 
 def tentang_menu():
-    st.markdown("<h3 style='text-align: center;'> Ruang Vektor </h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis DNR!</h3>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor Berbasis</h2>", unsafe_allow_html=True)
     
     st.write(" ")
-    st.markdown("<h4 style='text-align: left;'>Tentang Kami</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: left;'>Tentang Pengembang Website</h4>", unsafe_allow_html=True)
     st.write(" ")
     st.write("Kami adalah tim pengampu Mata Kuliah Aljabar Linier dan Matriks!")
-    st.write("👩‍🏫 Konten dan Programmer: Enjun Junaeti")
+    st.write("👩‍🏫 Konten Kreator dan Programmer: Enjun Junaeti")
     st.write('<p style="text-indent: 20px; ">📧 Email: enjun@upi.edu.</p>', unsafe_allow_html=True)
     st.write("👨‍⚖️ Supervisor: Prof. H. Yaya Sukjaya Kusumah, M.Sc., Ph. D.")
     st.write(" ")

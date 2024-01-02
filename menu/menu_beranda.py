318b4c45c980142e90f513aea1dd7c890fc5a085
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 11:30:31 +0700

    Update menu_beranda.py

--- menu/menu_beranda.py
+++ menu/menu_beranda.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def beranda_menu():
     # Judul aplikasi
-    st.markdown("<h3 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor</h3>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor</h1>", unsafe_allow_html=True)
     st.image("https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/Webinar-pana.png",width=600)
 
     st.markdown("<h4 style='text-align: center;'>👈 Pilih Menu Belajar dari Sidebar untuk memulai kegiatan pembelajaran.</h4>", unsafe_allow_html=True)

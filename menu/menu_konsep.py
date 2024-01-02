f50d29d222acfae8218c0d94450a53ecd473a6d5
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 12:15:54 +0700

    Update menu_konsep.py

--- menu/menu_konsep.py
+++ menu/menu_konsep.py
@@ -13,10 +13,10 @@ def konsep_menu():
 
   tab1, tab2, = st.tabs(["Tuple", "Vektor"])
 
- with tab1:
-   st.markdown("<h5 style='text-align: left;'>Tuple</h4>", unsafe_allow_html=True)
+   with tab1:
+     st.markdown("<h5 style='text-align: left;'>Tuple</h4>", unsafe_allow_html=True)
 
- with tab2:
-   st.markdown("<h5 style='text-align: left;'>Vektor</h4>", unsafe_allow_html=True)
+   with tab2:
+     st.markdown("<h5 style='text-align: left;'>Vektor</h4>", unsafe_allow_html=True)
   
  

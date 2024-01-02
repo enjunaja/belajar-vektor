44f1f6027926fb57f9d6f9b81cfd05c3f9bbb540
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 11:33:56 +0700

    Update menu_beranda.py

--- menu/menu_beranda.py
+++ menu/menu_beranda.py
@@ -4,7 +4,23 @@ import streamlit as st
 
 def beranda_menu():
     # Judul aplikasi
-    st.markdown("<h1 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor</h1>", unsafe_allow_html=True)
-    st.image("https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/Webinar-pana.png",width=600)
+    st.markdown("<h2 style='text-align: center;'> 💻 Media Pembelajaran Ruang Vektor</h2>", unsafe_allow_html=True)
+
+   # URL gambar dari "story set" atau sumber daya penyimpanan lainnya
+    image_url_story_set = "https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/Webinar-pana.png"
+
+    # Mendefinisikan CSS untuk menengahkan gambar
+    centered_image_style = (
+        "display: flex; justify-content: center; align-items: center; "
+        "width: 600;"  # Sesuaikan tinggi gambar sesuai kebutuhan Anda
+    )
+
+    # Menampilkan gambar dengan menggunakan style
+    st.markdown(
+        f'<div style="{centered_image_style}"><img src="{image_url_story_set}" alt="Gambar"></div>',
+        unsafe_allow_html=True
+    )
+
+    #st.image("https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/Webinar-pana.png",width=600)
 
     st.markdown("<h4 style='text-align: center;'>👈 Pilih Menu Belajar dari Sidebar untuk memulai kegiatan pembelajaran.</h4>", unsafe_allow_html=True)

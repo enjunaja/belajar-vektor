f05e3c667a6eb7748365078d8a40d6e7fc3983da
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-02 10:24:17 +0700

    Update menu_konsep.py

--- menu/menu_konsep.py
+++ menu/menu_konsep.py
@@ -4,22 +4,8 @@ def konsep_menu():
 
   tab1, tab2, tab3 = st.tabs(["Tujuan", "Kegiatan 1", "Kegiatan 2"])
 
-  option_data = [
-    {'icon': "🏠", 'label': Tujuan},
-    {'icon': "🤖", 'label': Kegiatan1}
-    {'icon': "🤖", 'label': Kegiatan2}
-
-  ]
-
   over_theme = {'txc_inactive': 'black', 'menu_background': '#F5B7B1', 'txc_active': 'white', 'option_active': '#CD5C5C'}
   font_fmt = {'font-class': 'h3', 'font-size': '50%'}
-
-  chosen_tab = hc.option_bar(
-    option_definition=option_data,
-    title='',
-    key='PrimaryOptionx',
-    override_theme=over_theme,
-    horizontal_orientation=True)
 
   with tab1: 
         st.markdown("<h5 style='text-align: left;'>Tujuan Pembelajaran</h4>", unsafe_allow_html=True)

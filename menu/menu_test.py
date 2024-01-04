42f7aa5de1a7247282f05ba1d1c9e1abd37355d5
Enjun Junaeti <95700781+enjunaja@users.noreply.github.com>
2024-01-04 10:27:04 +0700

    Update menu_test.py

--- menu/menu_test.py
+++ menu/menu_test.py
@@ -8,8 +8,17 @@ def test_menu():
     st.markdown("<h6 style='text-align: left;'>Mari kita cek pemahamanmu tentang Ruang Vektor!</h6>", unsafe_allow_html=True)
 
     tab1, tab2, tab3, tab4, tab5 = st.tabs(["Test 1", "Test 2", "Test 3", "Test 4", "Test 5"])
+    
     with tab1: 
-        st.markdown("<h5 style='text-align: left;'>Test 1</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: left;'>Latihan 1</h5>", unsafe_allow_html=True)
+        st.write("")
+        st.write("Perhatikan gambar berikut. Pada gambar terlihat bagaimana suatu vektor dapat digunakan untuk merepresentasikan warna.")
+        url_warna = "https://raw.githubusercontent.com/enjunaja/belajar-vektor/main/gambar/warna.png"
+        st.image(url_warna, caption='Merepresentasikan Warna dengan Vektor', width=50)
+        st.write("")
+        st.write("Apa yang dapat kamu ceritakan tentang gambar tersebut?")
+        st.text_input('Tuliskan pendapatmu!')
+        
     
     with tab2: 
         st.markdown("<h5 style='text-align: left;'>Test 2</h5>", unsafe_allow_html=True)
